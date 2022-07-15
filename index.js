2feafb386343f85dfbc0909094756c2d772d2c74
juanchicco <juanchicco87@gmail.com>
2022-07-15 17:04:02 -0300

    port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config()
 const express = require("express")
 const app = express()
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT|| 5000
 const path = require("path")
 const hbs = require("express-handlebars")
 const cors = require("cors")
