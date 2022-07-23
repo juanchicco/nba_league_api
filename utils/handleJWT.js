835472c62933fa7da7de63292b8c9fe130d90967
juanchicco <juanchicco87@gmail.com>
2022-07-23 11:34:38 -0300

    jwtKey

--- utils/handleJWT.js
+++ utils/handleJWT.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken")
-const key = process.env.jwtKey
+const key = "r39'nfasf83querlo%#"
 
 const tokenSign = async(user, time) => {
     return jwt.sign(user,key, {expiresIn: time})
